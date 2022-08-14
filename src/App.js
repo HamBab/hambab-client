9a2f4f2b0bcaa8da231d30ca8924bd481e79630b
sohy19 <sohy19@hufs.ac.kr>
2022-08-14 21:24:48 +0900

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./App.css";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Header />}>
+					<Route index element={<Main />} />
+				</Route>
+				<Route path="*" element={<div>There's nothing here!</div>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
