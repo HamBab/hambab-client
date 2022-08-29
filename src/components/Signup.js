943a1508b84c98e80bea7083acdf1fcf51979533
sasha1107 <sasha981107@gmail.com>
2022-08-30 03:07:13 +0900

    fix: 버그 수정

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 // import axios from "axios";
 import { SignupSection, ButtonDiv } from "./styledComponent";
@@ -9,6 +10,54 @@ const Signup = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
+	const [nickname, setNickname] = useState("");
+	const [sex, setSex] = useState("");
+	const [year, setYear] = useState("");
+	const [month, setMonth] = useState("");
+	const [day, setDay] = useState("");
+	const [dayList, setDayList] = useState("");
+
+	const yearList = [];
+	const monthList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+	for (let i = 1900; i < 2005; i++) {
+		yearList.push(i);
+	}
+
+	useEffect(() => {
+		const temp = [];
+		if (
+			month == 1 ||
+			month == 3 ||
+			month == 5 ||
+			month == 7 ||
+			month == 8 ||
+			month == 10 ||
+			month == 12
+		) {
+			for (let i = 1; i <= 31; i++) {
+				temp.push(i);
+			}
+		} else if (month == 2) {
+			if (
+				(year % 4 == 0 && year % 100 != 0) ||
+				(year % 100 != 0 && year % 400 == 0)
+			) {
+				for (let i = 1; i <= 29; i++) {
+					temp.push(i);
+				}
+			} else {
+				for (let i = 1; i <= 28; i++) {
+					temp.push(i);
+				}
+			}
+		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			for (let i = 1; i <= 30; i++) {
+				temp.push(i);
+			}
+		}
+		setDayList(temp);
+	}, [month, year]);
 
 	const handleSignUpButton = async () => {
 		// try {
@@ -43,6 +92,22 @@ const Signup = () => {
 
 	const onConfirmPasswordHandler = (event) => {
 		setConfirmPassword(event.currentTarget.value);
+	};
+
+	const onNicknameHandler = (event) => {
+		setNickname(event.currentTarget.value);
+	};
+	const onSexHandler = (event) => {
+		setSex(event.currentTarget.value);
+	};
+	const onYearHandler = (event) => {
+		setYear(event.currentTarget.value);
+	};
+	const onMonthHandler = (event) => {
+		setMonth(event.currentTarget.value);
+	};
+	const onDayHandler = (event) => {
+		setDay(event.currentTarget.value);
 	};
 
 	return (
@@ -96,8 +161,6 @@ const Signup = () => {
 						className="loginregister__input"
 					/>
 				</div>
-<<<<<<< Updated upstream
-=======
 				<div id="info__birth">
 					<select id="birth-year" onChange={onYearHandler}>
 						<option disabled selected>
@@ -149,7 +212,6 @@ const Signup = () => {
 						여
 					</section>
 				</div>
->>>>>>> Stashed changes
 				<ButtonDiv className="d-grid gap-2">
 					{confirmPassword.length === 0 ||
 						name.length === 0 ||
