466f47ad1281d850b050bd5181189f97dae0397b
Soohyun Jung <sasha1107@naver.com>
2022-09-14 22:25:00 +0900

    FIX: 충돌 수정

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import RestaurantList from "./components/RestaurantList";
 import MakeGroup from "./components/MakeGroup";
-<<<<<<< HEAD
-import Chat from "./components/chat/Chat";
-=======
 import EditGroup from "./components/EditGroup";
 import MyPage from "./components/MyPage"
->>>>>>> 077b5ed9a77178b1202083ec9c161b5e7337bb3d
+import Chat from "./components/chat/Chat";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -25,22 +23,17 @@ function App() {
 			<Routes>
 				<Route
 					path="/"
-					element={<Navbar onSelect={onSelect} location={location} />}
-				>
+					element={<Navbar onSelect={onSelect} location={location} />} />
 					<Route index element={<Main onSelect={onSelect} />} />
 					<Route path="restaurants/:category" element={<RestaurantList />} />
 					<Route path="restaurants/detail" element={<Detail />} />
 					<Route path="restaurants/makegroup" element={<MakeGroup />} />
 					<Route path="restaurants/editgroup" element={<EditGroup />} />
+					<Route path="/restaurants/chat" element={<Chat />} />  
 					<Route path="/login" element={<Login />} />                        
 					<Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
-					<Route path="/restaurants/chat" element={<Chat />} />
-=======
-					<Route path="/mypage" element={<MyPage />} />              
->>>>>>> 077b5ed9a77178b1202083ec9c161b5e7337bb3d
-				</Route>
-				<Route path="*" element={<div>There's nothing here!</div>} />
+					<Route path="/mypage" element={<MyPage />} />
+					<Route path="*" element={<div>There's nothing here!</div>} />
 			</Routes>
 		</div>
 	);
